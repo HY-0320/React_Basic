a0d0383c80847f82389353fda1df05e154b1e273
HY-0320 <418817150@qq.com>
2021-06-11 10:28:04 +0800

    6.11

--- src/components/Home/Message/Detail/Detail.jsx
+++ src/components/Home/Message/Detail/Detail.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+// import qs from 'querystring'
 
 const detailData = [{id:'01',content:"黄越"},{id:'02',content:"5120186065"},{id:'03',content:"西南科技大学"}]
 export default class Detail extends Component {
   render() {
     console.log(this.props)
-    const {id,title} = this.props.match.params
+
+    // 接收parmas参数
+    // const {id,title} = this.props.match.params
+
+    //接收search参数 需要借助querystring
+    // const {search} = this.props.location;
+    // const {id,title} = qs.parse(search.slice(1))
+
+    //接收state参数
+    const {id,title} = this.props.location.state
     return (
       <ul>
         <li>ID:{id}</li>
--- src/components/Home/Message/Message.jsx
+++ src/components/Home/Message/Message.jsx
@@ -19,6 +19,40 @@ export default class Message extends Component {
       }
     ]
   }
+	replaceShow = (id,title)=>{
+		//replace跳转+携带params参数
+		//this.props.history.replace(`/home/message/detail/${id}/${title}`)
+
+		//replace跳转+携带search参数
+		// this.props.history.replace(`/home/message/detail?id=${id}&title=${title}`)
+
+		//replace跳转+携带state参数
+		this.props.history.replace(`/home/message/detail`,{id,title})
+	}
+
+	pushShow = (id,title)=>{
+		//push跳转+携带params参数
+		// this.props.history.push(`/home/message/detail/${id}/${title}`)
+
+		//push跳转+携带search参数
+		// this.props.history.push(`/home/message/detail?id=${id}&title=${title}`)
+
+		//push跳转+携带state参数
+		this.props.history.push(`/home/message/detail`,{id,title})
+		
+	}
+
+	back = ()=>{
+		this.props.history.goBack()
+	}
+
+	forward = ()=>{
+		this.props.history.goForward()
+	}
+
+	go = ()=>{
+		this.props.history.go(-2)
+	}
   render() {
     const {messageArry} = this.state
     return (
@@ -28,15 +62,35 @@ export default class Message extends Component {
             messageArry.map(item =>{
               return (
               <li key={item.id}>
-                <Link to = {`/home/message/detail/${item.id}/${item.title}`}>{item.title}</Link>
+                {/* 传递parmas参数 */}
+                {/* <Link to = {`/home/message/detail/${item.id}/${item.title}`}>{item.title}</Link> */}
+
+                {/* 传递search参数 */}
+                {/* <Link to = {`/home/message/detail/?id=${item.id}&title=${item.title}`}>{item.title}</Link> */}
+
+                {/* 传递state参数 */}
+                <Link replace to={{pathname:'/home/message/detail',state:{id:item.id,title:item.title}}}>{item.title}</Link>
+                {/* 默认history.push方式 通过压栈来保存路由 replace = {true} 通过replace替换来保存 */}
+                &nbsp;<button onClick={()=> this.pushShow(item.id,item.title)}>push查看</button>
+								&nbsp;<button onClick={()=> this.replaceShow(item.id,item.title)}>replace查看</button>
               </li>)
             } )
           }
         </ul>
         <hr/>
         <Switch>
-          <Route path="/home/message/detail/:id/:title" component={Detail}></Route>
+          {/* 声明接收params参数 */}
+          {/* <Route path="/home/message/detail/:id/:title" component={Detail}></Route> */}
+
+          {/* search参数无需声明接收，正常注册路由即可  */}
+          {/* <Route path="/home/message/detail" component={Detail}></Route> */}
+
+          {/* state参数无需声明接收，正常注册路由即可 */}
+          <Route path='/home/message/detail' component={Detail}></Route>
         </Switch>
+        <button onClick={this.back}>回退</button>&nbsp;
+				<button onClick={this.forward}>前进</button>&nbsp;
+				<button onClick={this.go}>go</button>
       </div>
     );
   }
